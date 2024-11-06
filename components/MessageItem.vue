a4e22a26de89b7dfe2faf74297dec37643335467
demaxl <aweabdullahi@gmail.com>
2024-11-06 21:02:56 +0100

    add basic layout of chat view

--- components/MessageItem.vue
+++ components/MessageItem.vue
@@ -1,43 +1,47 @@
 <template>
-    <div
-        class="flex items-center text-glare-gray transition-colors cursor-pointer"
-    >
-        <div class="flex items-center flex-grow">
-            <img
-                class="mr-3 w-9 h-9"
-                src="/images/default_message_icon.png"
-                alt="Message Icon"
-            />
-            <div class="flex flex-col truncate">
-                <div>
-                    <span
-                        class="mr-2 text-body-1 font-semibold text-glare-black"
-                        >Suzana Colin</span
+    <nuxt-link to="/chat" class="block">
+        <div
+            class="flex items-center text-glare-gray transition-colors cursor-pointer"
+        >
+            <div class="flex items-center flex-grow">
+                <img
+                    class="mr-3 w-9 h-9"
+                    src="/images/default_message_icon.png"
+                    alt="Message Icon"
+                />
+
+                <div class="flex flex-col truncate">
+                    <div>
+                        <span
+                            class="mr-2 text-body-1 font-semibold text-glare-black"
+                            >Suzana Colin</span
+                        >
+                        <span class="text-button-3">@suzana</span>
+                    </div>
+                    <p
+                        class="text-[10px] leading-4 truncate md:max-w-[250px] max-w-[250px] sm:max-w-[500px]"
                     >
-                    <span class="text-button-3">@suzana</span>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Itaque dicta id, est quaerat, ea perspiciatis suscipit
+                        quas, quis quisquam magnam aspernatur officiis. Esse
+                        harum tempore eum consequuntur consectetur itaque dolore
+                        saepe quos, tempora illum magni modi reprehenderit
+                        beatae officia vel officiis fugit asperiores ratione!
+                        Exercitationem, voluptatum deserunt nisi hic corrupti
+                        qui modi, quis accusantium accusamus provident
+                        voluptatibus dolore, dicta commodi quos earum! Sed
+                        doloribus rerum fugit? Quisquam nobis iusto similique
+                        minima amet reiciendis sed, quae, optio numquam error ad
+                        est magnam! Mollitia qui veniam porro assumenda non
+                        delectus officia. Molestiae deleniti soluta pariatur
+                        veniam voluptate aspernatur, laboriosam possimus
+                        eligendi sed.
+                    </p>
                 </div>
-                <p
-                    class="text-[10px] leading-4 truncate md:max-w-[250px] max-w-[250px] sm:max-w-[500px]"
-                >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Itaque dicta id, est quaerat, ea perspiciatis suscipit quas,
-                    quis quisquam magnam aspernatur officiis. Esse harum tempore
-                    eum consequuntur consectetur itaque dolore saepe quos,
-                    tempora illum magni modi reprehenderit beatae officia vel
-                    officiis fugit asperiores ratione! Exercitationem,
-                    voluptatum deserunt nisi hic corrupti qui modi, quis
-                    accusantium accusamus provident voluptatibus dolore, dicta
-                    commodi quos earum! Sed doloribus rerum fugit? Quisquam
-                    nobis iusto similique minima amet reiciendis sed, quae,
-                    optio numquam error ad est magnam! Mollitia qui veniam porro
-                    assumenda non delectus officia. Molestiae deleniti soluta
-                    pariatur veniam voluptate aspernatur, laboriosam possimus
-                    eligendi sed.
-                </p>
             </div>
+            <div class="text-[10px] leading-4 pl-2 text-nowrap">Dec 15</div>
         </div>
-        <div class="text-[10px] leading-4 pl-2 text-nowrap">Dec 15</div>
-    </div>
+    </nuxt-link>
 </template>
 
 <!-- <script setup>
---
+++ pages/chat.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="h-screen px-9 relative">
+        <div class="py-3">
+            <div class="flex space-x-3">
+                <img
+                    class="w-9 h-9"
+                    src="/images/default_message_icon.png"
+                    alt="Message Icon"
+                />
+                <div class="flex flex-col">
+                    <span class="text-body-1 font-semibold text-glare-black"
+                        >Suzana Colin</span
+                    >
+                    <span class="text-caption text-glare-gray">@suzana</span>
+                </div>
+            </div>
+        </div>
+
+        <div
+            class="h-16 flex items-center space-x-11 py-5 mx-9 absolute right-0 left-0 bottom-0"
+        >
+            <button
+                class="text-primary px-3 py-2 rounded hover:bg-[#eee] transition-colors"
+            >
+                <Icon
+                    class="align-middle"
+                    name="uil:image-upload"
+                    size="24px"
+                />
+            </button>
+            <input
+                type="text"
+                class="shadow-[0px_0px_1px_0px_#00000040] focus:outline-primary border block grow rounded-3xl px-6 py-3 h-10 max-w-[850px] text-body-2"
+                placeholder="Type a messsage"
+            />
+
+            <button
+                class="text-primary px-3 py-2 rounded hover:bg-[#eee] transition-colors"
+            >
+                <Icon class="align-middle" name="iconoir:send" size="24px" />
+            </button>
+        </div>
+    </div>
+</template>
