e686f1c73d988425c360c50a3f20be2225bf0881
demaxl <aweabdullahi@gmail.com>
2024-11-06 21:02:34 +0100

    show sidebar conditionally based on route
    change Icon library to nuxt/icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,24 +2,24 @@
     <div class="w-full">
         <div class="flex h-screen">
             <div
+                :class="{ 'hidden md:block': isChatView }"
                 class="bg-[#FAFAFA] basis-full md:max-w-[400px] md:basis-1/4 p-6 pr-2"
             >
-                <NuxtIcon
-                    name="logo"
-                    class="w-[150px] block h-[43px] mb-[88px]"
-                    :filled="true"
-                    :fontControlled="false"
-                />
+                <nuxt-link to="/">
+                    <Icon
+                        name="icon:logo"
+                        class="w-[150px] block h-[43px] mb-[88px]"
+                    />
+                </nuxt-link>
 
                 <p class="text-subtitle-1 font-semibold mb-7 text-black">
                     Messages
                 </p>
 
                 <div class="pr-6 mb-10 relative">
-                    <NuxtIcon
-                        name="search"
+                    <Icon
+                        name="icon:search"
                         class="block absolute top-3 left-4"
-                        filled
                     />
                     <input
                         class="mx-auto text-button-2 block w-full rounded-3xl border placeholder:text-[#C7C3C3] border-[#E8E8E8] py-3 pl-14 pr-9"
@@ -33,12 +33,17 @@
                     <MessageItem />
                 </div>
             </div>
-            <div class="hidden md:block md:basis-3/4">
+            <div :class="{ hidden: !isChatView }" class="md:block md:basis-3/4">
                 <slot />
             </div>
         </div>
     </div>
 </template>
+
+<script setup>
+const route = useRoute();
+const isChatView = computed(() => route.name === "chat");
+</script>
 
 <style>
 .nuxt-icon svg {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "nuxt-icons"],
+    modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
     css: ["~/assets/css/fonts.css", "animate.css/animate.min.css"],
+    icon: {
+        customCollections: [
+            {
+                prefix: "icon",
+                dir: "./assets/icons",
+            },
+        ],
+    },
 });
