35c2b0477de6c8b17df0bb8ef5d0c99a95d2e9e5
demaxl <aweabdullahi@gmail.com>
2024-11-18 15:48:00 +0100

    add sign up view

--- frontend/pages/signup.vue
+++ frontend/pages/signup.vue
@@ -4,14 +4,14 @@
             class="mx-auto text-center bg-[#FAFAFA] tracking-wide p-8 rounded-2xl shadow-[0_0_20px_rgba(0,0,0,0.1)] w-[500px] max-w-xl"
         >
             <Icon size="40px" name="icon:logo" class="block mx-auto mb-10" />
-            <p class="font-bold text-subtitle-1 mb-3">Welcome Back</p>
+            <p class="font-bold text-subtitle-1 mb-3">Create an account</p>
 
-            <p class="text-body-2 text-gl-glare-blacktracking-normal">
-                Don't have an account yet?
+            <p class="text-body-2 text-glare-gray tracking-normal">
+                Already have an account?
                 <nuxt-link
-                    to="/signup"
+                    to="/login"
                     class="font-semibold text-black hover:underline"
-                    >Sign up</nuxt-link
+                    >Log in</nuxt-link
                 >
             </p>
 
@@ -29,7 +29,7 @@
                         placeholder="Username"
                     />
                 </div>
-                <div class="pr-6 mb-6 relative">
+                <div class="pr-6 mb-4 relative">
                     <Icon
                         size="20px"
                         name="uil:padlock"
@@ -42,10 +42,23 @@
                         placeholder="Password"
                     />
                 </div>
+                <div class="pr-6 mb-6 relative">
+                    <Icon
+                        size="20px"
+                        name="uil:padlock"
+                        class="block absolute top-3 left-4"
+                    />
+                    <input
+                        class="mx-auto text-button-2 block w-full border rounded-lg py-2 pl-14 pr-9"
+                        type="password"
+                        aria-label="Confirm password"
+                        placeholder="Confirm password"
+                    />
+                </div>
 
                 <input
                     type="submit"
-                    value="Login"
+                    value="Sign up"
                     class="block cursor-pointer hover:bg-glare-dark-green transition-colors tracking-wide text-button-2 w-full mx-auto rounded-lg p-2 bg-glare-green text-white"
                 />
 
