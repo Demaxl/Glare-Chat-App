727bc15dbc10281b38b3f0070b049bc12f988b3d
demaxl <aweabdullahi@gmail.com>
2024-11-19 19:53:09 +0100

    add form validation for login form

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@vueuse/nuxt"],
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@nuxt/icon",
+        "@vueuse/nuxt",
+        "@vee-validate/nuxt",
+    ],
     css: ["~/assets/css/fonts.css", "animate.css/animate.min.css"],
     icon: {
         customCollections: [
--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -15,18 +15,29 @@
                 >
             </p>
 
-            <form action="#" class="mt-8">
+            <Form
+                @submit="onSubmit"
+                :validation-schema="schema"
+                :initial-values="initialValues"
+                class="mt-8"
+                v-slot="{ isSubmitting, meta }"
+            >
                 <div class="pr-6 mb-4 relative">
                     <Icon
                         size="20px"
                         name="uil:envelope"
                         class="block absolute top-3 left-4"
                     />
-                    <input
+                    <Field
+                        name="username"
                         class="mx-auto text-button-2 block w-full border rounded-lg py-2 pl-14 pr-9"
                         type="text"
                         aria-label="username"
                         placeholder="Username"
+                    />
+                    <ErrorMessage
+                        name="username"
+                        class="text-red-500 text-start block text-sm mt-2"
                     />
                 </div>
                 <div class="pr-6 mb-6 relative">
@@ -35,18 +46,24 @@
                         name="uil:padlock"
                         class="block absolute top-3 left-4"
                     />
-                    <input
+                    <Field
                         class="mx-auto text-button-2 block w-full border rounded-lg py-2 pl-14 pr-9"
                         type="password"
                         aria-label="password"
+                        name="password"
                         placeholder="Password"
                     />
                 </div>
 
                 <input
                     type="submit"
+                    :disabled="
+                        !meta.dirty ||
+                        !(meta.touched && meta.valid) ||
+                        isSubmitting
+                    "
                     value="Login"
-                    class="block cursor-pointer hover:bg-glare-dark-green transition-colors tracking-wide text-button-2 w-full mx-auto rounded-lg p-2 bg-glare-green text-white"
+                    class="block disabled:cursor-not-allowed disabled:bg-glare-gray cursor-pointer hover:bg-glare-dark-green transition-colors tracking-wide text-button-2 w-full mx-auto rounded-lg p-2 bg-glare-green text-white"
                 />
 
                 <div class="flex items-center gap-4 w-full my-4">
@@ -62,7 +79,29 @@
                     value="Continue with Google"
                     class="block cursor-pointer border-glare-black transition-colors tracking-wide text-button-2 w-full mx-auto rounded-lg p-2 border"
                 />
-            </form>
+            </Form>
         </div>
     </div>
 </template>
+
+<script setup>
+import { set } from "@vueuse/core";
+import { object, string } from "yup";
+
+const initialValues = {
+    username: "",
+    password: "",
+};
+
+const schema = object({
+    username: string().required("Required"),
+    password: string().required(),
+});
+
+async function onSubmit(values, { setErrors }) {
+    await new Promise((r) => setTimeout(r, 1000));
+    setErrors({ username: "Invalid username" });
+    console.log(values);
+    // alert(values);
+}
+</script>
