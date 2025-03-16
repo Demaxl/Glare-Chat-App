54da786e1bd0036b5cc82bd965f7be435232f25a
demaxl <aweabdullahi@gmail.com>
2025-03-16 15:43:10 +0100

    add profile div in chat layout

--- frontend/layouts/chat.vue
+++ frontend/layouts/chat.vue
@@ -3,39 +3,66 @@
         <div class="flex h-screen">
             <div
                 :class="{ 'hidden md:block': isChatView }"
-                class="bg-[#FAFAFA] basis-full md:max-w-[400px] md:basis-1/4 p-6 pr-2"
+                class="bg-[#FAFAFA] basis-full flex flex-col justify-between md:max-w-[400px] md:basis-1/4 p-6 pr-2 relative"
             >
-                <nuxt-link to="/">
-                    <Icon
-                        name="icon:logo"
-                        class="w-[150px] block h-[43px] mb-[88px]"
-                    />
-                </nuxt-link>
-
-                <p class="text-subtitle-1 font-semibold mb-7 text-black">
-                    Messages
-                </p>
-
-                <div class="pr-6 mb-10 relative">
-                    <Icon
-                        name="icon:search"
-                        class="block absolute top-3 left-4"
-                    />
-                    <input
-                        class="mx-auto text-button-2 block w-full rounded-3xl border placeholder:text-[#C7C3C3] border-[#E8E8E8] py-3 pl-14 pr-9"
-                        type="text"
-                        placeholder="Search people"
-                        @input="debouncedUserSearch"
-                        v-model="searchQuery"
-                    />
+                <div>
+                    <nuxt-link to="/">
+                        <Icon
+                            name="icon:logo"
+                            class="w-[150px] block h-[43px] mb-[88px]"
+                        />
+                    </nuxt-link>
+                    <p class="text-subtitle-1 font-semibold mb-7 text-black">
+                        Messages
+                    </p>
+                    <div class="pr-6 mb-10 relative">
+                        <Icon
+                            name="icon:search"
+                            class="block absolute top-3 left-4"
+                        />
+                        <input
+                            class="mx-auto text-button-2 block w-full rounded-3xl border placeholder:text-[#C7C3C3] border-[#E8E8E8] py-3 pl-14 pr-9"
+                            type="text"
+                            placeholder="Search people"
+                            @input="debouncedUserSearch"
+                            v-model="searchQuery"
+                        />
+                    </div>
+                    <div class="space-y-6">
+                        <ChatListItem
+                            v-for="(message, index) in recentContacts"
+                            :key="index"
+                            v-bind="message"
+                        />
+                    </div>
                 </div>
-
-                <div class="space-y-6">
-                    <ChatListItem
-                        v-for="(message, index) in recentContacts"
-                        :key="index"
-                        v-bind="message"
-                    />
+                <div class="flex justify-between">
+                    <div class="flex">
+                        <img
+                            class="w-9 h-9 mr-3"
+                            src="/images/default_message_icon.png"
+                            alt="User profile picture"
+                        />
+                        <div class="flex flex-col">
+                            <span
+                                class="text-body-1 font-semibold text-glare-black"
+                                >{{ userData.username }}</span
+                            >
+                            <span class="text-caption text-glare-gray"
+                                >@{{ userData.username }}</span
+                            >
+                        </div>
+                    </div>
+                    <button
+                        class="p-3 rounded-full font-bold hover:bg-[#eee] transition-colors"
+                        @click="sendMessage"
+                    >
+                        <Icon
+                            class="align-middle"
+                            name="iconoir:more-horiz"
+                            size="24px"
+                        />
+                    </button>
                 </div>
             </div>
             <div
